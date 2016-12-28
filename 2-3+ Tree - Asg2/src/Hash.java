76fcbc3e4ab4d2f3e32fee55c51a59a3ad40476d
Jack Guttman <jackrg94@vt.edu>
2016-12-27 22:02:58 -0500

    Update Hash.java

--- 2-3+ Tree - Asg2/src/Hash.java
+++ 2-3+ Tree - Asg2/src/Hash.java
@@ -58,9 +58,6 @@ public class Hash {
      * @param m The size of the hash table
      * @return The home slot for that string
      */
-    // Make this private in your project.
-    // This is private for distributing hash function in a way that will
-    // pass milestone 1 without change.
     private int h(String s, int m) {
         int intLength = s.length() / 4;
         long sum = 0;
